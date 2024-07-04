f021ce4a294329c9fe45dfe719722e106822c4e7
Fran Rodriguez <fr.rodmar@gmail.com>
2024-07-04 20:58:15 +0100

    Refactor Layout component to use CSS grid for responsive layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,23 +4,45 @@ import Footer from "./Footer.jsx";
 import styled from "styled-components";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-areas:
+    "menu"
+    "body"
+    "footer";
   min-height: 100vh;
+
+  @media (${(props) => props.theme.breakpoints.mobile}) {
+    grid-template-areas:
+      "body"
+      "footer"
+      "menu";
+  }
 `;
 
-const Main = styled.main`
-  flex: 1;
+const Menu = styled.div`
+  grid-area: menu;
+`;
+
+const Body = styled.main`
+  grid-area: body;
+`;
+
+const CustomFooter = styled.div`
+  grid-area: footer;
 `;
 
 const Layout = ({ children }) => {
   return (
     <Container>
-      <Header />
-      <Main>
-        { children }
-      </Main>
-      <Footer />
+      <Menu>
+        <Header />
+      </Menu>
+      <Body>
+        {children}
+      </Body>
+      <CustomFooter>
+        <Footer />
+      </CustomFooter>
     </Container>
   );
 };

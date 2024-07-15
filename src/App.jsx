11b1beb930fa71f1da14ebcdc7044c96a529429b
Fran Rodriguez <fr.rodmar@gmail.com>
2024-07-15 18:25:50 +0100

    Add id for projects titles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hello from "./components/sections/Hello";
 import Projects from "./components/sections/Projects";
 import projectsData from "./data/projectsData";
 import oldprojectsData from "./data/oldprojectsData";
+import About from "./components/sections/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hello />
       <Projects projectsData={projectsData} sectionType="New" />
       <Projects projectsData={oldprojectsData} sectionType="Old" />
+      <About />
     </>
   );
 }
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 function About() {
   return (
-    <></>
+    <>
+      <h1>Test</h1>
+    </>
   );
 }
 

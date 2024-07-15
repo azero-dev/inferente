d268523b8c841c0e49c61a7429e709cfc5e3ac6b
Fran Rodriguez <fr.rodmar@gmail.com>
2024-07-15 23:54:36 +0100

    Create About component layout and styles

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,10 +1,84 @@
 import styled from "styled-components";
+import SectionContainer from "../common/SectionContainer";
+import aboutData from "../../data/aboutData";
+
+const hoverEffect = `
+  border-radius: 10px;
+  padding: 2em;
+  transition: all 0.2s;
+  transition-timing-function: ease-out;
+  
+  &:hover {
+    transform: scale(1.02);
+    background-color: #0a0e1d;
+  }
+`;
+
+const AboutContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, auto);
+  grid-template-areas:
+    "intro intro intro"
+    "exp edu skills";
+  justify-items: center;
+`
+
+const AboutIntro = styled.div`
+  grid-area: intro;
+`
+
+const AboutExp = styled.div`
+  grid-area: exp;
+  ${hoverEffect}
+`
+
+const AboutEdu = styled.div`
+  grid-area: edu;
+  ${hoverEffect}
+`
+
+const AboutSkills = styled.div`
+  grid-area: skills;
+  ${hoverEffect}
+`
+
+
 
 function About() {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <SectionContainer>
+      <h1 id="about">About me</h1>
+      <AboutContainer>
+        <AboutIntro>
+          <p>{aboutData.intro}</p>
+        </AboutIntro>
+        <AboutExp>
+          <h3>Experience</h3>
+          <ul>
+            {aboutData.experience.map((exp, index) => (
+              <li key={index}>{exp}</li>
+            ))}
+          </ul>
+        </AboutExp>
+        <AboutEdu>
+          <h3>Education</h3>
+          <ul>
+            {aboutData.education.map((edu, index) => (
+              <li key={index}>{edu}</li>
+            ))}
+          </ul>
+        </AboutEdu>
+        <AboutSkills>
+          <h3>Skills</h3>
+          <ul>
+            {aboutData.skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </AboutSkills>
+      </AboutContainer>
+    </SectionContainer>
   );
 }
 
---
+++ src/data/aboutData.js
@@ -0,0 +1,8 @@
+const aboutData = {
+  intro: 'This is just a little bit about me. I am a software engineer with a passion for web development. I have experience with React, Node.js, and Express.js. I am always looking to learn new things and improve my skills.',
+  experience: ['experience 1', 'experience 2', 'experience 3'],
+  education: ['education 1', 'education 2', 'education 3'],
+  skills: ['skill 1', 'skill 2', 'skill 3']
+}
+
+export default aboutData

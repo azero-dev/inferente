a331c2b922e6ebe06d52e766a9498c9461c3195a
Fran Rodriguez <fr.rodmar@gmail.com>
2024-07-22 19:42:13 +0100

    refactor: Remove unnecessary code in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL,
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
 })
